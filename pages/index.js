98cb5e4e5baa2efa83d5fdf5cd8ff5061e479887
Luciano Junior <engluciano.junior@gmail.com>
2021-08-31 18:52:58 -0300

    Navbar

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,21 @@ function Home(){
                 <link rel="shortcut icon" href="images/favicon.ico" />
                   <title> Celke Imersão 11 </title>
               </Head>
+              <nav className="navbar">
+            <div class="max-width">
+                <div className="logo">
+                    <a href="#top">Luciano Lima Jr</a>
+                </div>
+                <ul className="menu">
+                    <li><a href="#top" class="menu-btn">Home</a></li>
+                    <li><a href="#services" class="menu-btn">Serviços</a></li>
+                    <li><a href="#contact" class="menu-btn">Contato</a></li>
+                </ul>
+                <div className="menu-btn">
+                    <i className="fas fa-bars"></i>
+                </div>
+            </div>
+        </nav>
         
     </div>
     );
