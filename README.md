5c919398526170428ad8c127a628968fdc10ecee
Luciano Junior <engluciano.junior@gmail.com>
2021-08-31 18:31:49 -0300

    Head

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 Imersão react 11 com Prof Celke
 
 31/08/2021
+
+COMO RODAR PROJETO BAIXADO
+instalar todas as dependencias indicada pelo arquivo package.json
+### npm install
+
+RODAR  PROJETO
+### npm run dev
+
+SEQUENCI DO PROJETO COM NEXT.JS
+### npm install next react-dom
+
+RODAR O PROJETO 
+### npm run dev
+
+ACESSAR O PROJETO PELO NAVEGADOR
+### localhost:3000
+
---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Home(){
+    return (
+         <div>
+              <Head>
+              <meta charSet="UTF-8"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <link rel="shortcut icon" href="images/favicon.ico" />
+                  <title> Celke Imersão 11 </title>
+              </Head>
+        
+    </div>
+    );
+}
+
+export default Home;
